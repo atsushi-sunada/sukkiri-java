fcecec9751f55fe418ea227d6cc6b81eee6179a3
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-03-23 01:57:11 +0900

    20230323_FeedBackByFunatsu
    問題の要件に合うように、所属させるパッケージを考えることがポイントになります。
    問題文では「デフォルトパッケージに属する」と書いていますので、commentパッケージとの関係を考えて、所属させるパッケージを決めてみましょう
    （commentパッケージと別のパッケージにすることが重要です）

--- src/chapter06/task6_1/comment/Main.java
+++ src/chapter06/task6_1/comment/Main.java
@@ -1,8 +1,8 @@
-package chapter06.task6_1.comment;
+package chapter06.task6_1.comment;    // Funatsu:問題文によると、Mainクラスはcommentパッケージに配置しないので、commentパッケージ以外に配置しましょう
 public class Main {
 	public static void main(String[] args) {
-		chapter06.task6_1.comment.Zenhan.doWarusa();
-		chapter06.task6_1.comment.Zenhan.doTogame();
+		chapter06.task6_1.comment.Zenhan.doWarusa();     // Funatsu:練習6-1③の内容と異なりますので、確認してみましょう
+		chapter06.task6_1.comment.Zenhan.doTogame();     // Funatsu:練習6-1③の内容と異なりますので、確認してみましょう
 		chapter06.task6_1.comment.Kouhan.callDeae();
 		chapter06.task6_1.comment.Kouhan.showMondokoro();
 	}
