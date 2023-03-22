ed5e52abe7815c001ecae0399bc7664f875b2e32
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-03-23 01:35:03 +0900

    20230323_FeedBackByFunatsu

--- src/chapter03/Task3_4.java
+++ src/chapter03/Task3_4.java
@@ -1,6 +1,7 @@
 package chapter03;
 
 public class Task3_4 {
+	// Funatsu:字下げ文字をタブか半角スペースどちらかに揃えましょう
 	public static void main(String[] args) { 
 	    boolean tenki = true;
 	    if(tenki == true) {
