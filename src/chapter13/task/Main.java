76ab5770455107b873018b83427f07e6f01c3316
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-03-23 02:36:09 +0900

    20230323_FeedBackByFunatsu

--- src/chapter13/task/Main.java
+++ src/chapter13/task/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Wizad w = new Wizad();
 		Hero h = new Hero();
-		w.setName("");
+		w.setName("");     // Funatsu: コードの動きとは関係ないですが、名無しはさすがに寂しいです
 		
 		
 	}
