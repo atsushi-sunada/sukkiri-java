405997c2fc4d696bf4adcf7990d118b1b7bf8b1a
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-03-23 01:31:01 +0900

    20230323_FeedBackByFunatsu

--- src/chapter02/Task2_1.java
+++ src/chapter02/Task2_1.java
@@ -1,6 +1,7 @@
 package chapter02;
 
 public class Task2_1 {
+	// Funatsu: 細かい話になるのですが、字下げはタブ文字と半角スペースどちらかに揃えましょう。表示させる環境によって、字下げ幅がバラバラになることがあります。
 	public static void main(String[] args) { 
 	    int x = 5;
 	    int y = 10;
