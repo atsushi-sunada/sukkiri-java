bf8c61474bde6e7c5bb904f1776f7ee7776e095b
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-03-23 01:42:15 +0900

    20230323_FeedBackByFunatsu
    一つ課題を追加しました。

--- src/chapter04/Task4_1.java
+++ src/chapter04/Task4_1.java
@@ -7,6 +7,7 @@ public class Task4_1 {
 		boolean [] answers = new boolean[3];
 		String [] names = new String[3];
 		
+		// 下記のような処理を追加されたのは良いですね！それぞれの値が何の変数か？がわかる表示にしてみましょう
 		System.out.println(points.length);
 		System.out.println(weghts.length);
 		System.out.println(answers.length);
