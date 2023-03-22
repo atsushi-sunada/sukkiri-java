6742781ad2363eabdfced4c3a31998feecd7a12d
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-03-23 01:36:15 +0900

    20230323_FeedBackByFunatsu

--- src/chapter03/Task3_5.java
+++ src/chapter03/Task3_5.java
@@ -1,6 +1,7 @@
 package chapter03;
 
 public class Task3_5 {
+	// Funatsu:字下げ文字をタブか半角スペースどちらかに揃えましょう
 	public static void main(String[] args) { 
 	    System.out.println("[メニュー]１：検索　２：登録　３：削除　４：変更");
 	    int selected = new java.util.Scanner(System.in).nextInt();
@@ -17,7 +18,7 @@ public class Task3_5 {
 	        case 4:
 	            System.out.println("変更します");
 	            break;
-	        
+	        // 1〜4以外の時だけ「1〜4を選ばないと、何もしないよ！」を表示させてください
 	    }
 	}
 
