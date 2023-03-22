f351a1802575791927187f51751bf660bd8dc466
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-03-23 01:28:50 +0900

    20230323_FeedBackByFunatsu

--- src/chapter01/Task1_3.java
+++ src/chapter01/Task1_3.java
@@ -1,6 +1,7 @@
 package chapter01;
 
 public class Task1_3 {
+	// Funatsu: このコードはコンパイルエラーになるはずです。動かしても何も起きない内容ですが、一応メソッドで囲んであげてください。
 		    boolean result = true;
 		    char charcter = '駆';
 		    double few = 3.14;
