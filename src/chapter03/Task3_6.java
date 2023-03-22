5bef154b8ba91542f8a6f03c7b8bc998a60df376
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-03-23 01:39:42 +0900

    20230323_FeedBackByFunatsu

--- src/chapter03/Task3_6.java
+++ src/chapter03/Task3_6.java
@@ -1,10 +1,11 @@
 package chapter03;
 
 public class Task3_6 {
+	// Funatsu:字下げ文字をタブか半角スペースどちらかに揃えましょう
 	public static void main(String[] args) { 
 	    System.out.println("【数あてゲーム】");
-	    int ans = new java.util.Random().nextInt(9);
-	    for(int i = 0; i<5; i++){
+	    int ans = new java.util.Random().nextInt(9);    // Funatsu: ここ、0〜9にならないはずです。数値を見直してみましょう。入門p91〜p92を参照
+	    for(int i = 0; i<5; i++){    // Funatsu:ここも細かいのですが、「i < 5」のようにオペランド間に半角スペースを入れましょう
 	        System.out.println("0~9の数字を入力してください");
 	        int num = new java.util.Scanner(System.in).nextInt();
 	        if(ans == num){
