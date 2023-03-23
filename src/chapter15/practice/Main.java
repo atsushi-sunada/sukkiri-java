74768019c19784b28015d8cf5aa7106ad1a175a3
atsushi-sunada <127069674 +atsushi-sunada@users.noreply.github.com>
2023-03-23 10:29:09 +0900

    課題・練習

---
+++ src/chapter15/practice/Main.java
@@ -0,0 +1,10 @@
+package chapter15.practice;
+
+public class Main {
+	public static void main(String[] args) {
+		String s1 = "Java programming";
+		
+		System.out.println("文字列の切り離し"+ s1.substring(3,8));
+	}
+
+}
