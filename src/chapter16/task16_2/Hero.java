057e4b32d02f219ab51f76c6c542e611aa057199
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-04-13 10:37:02 +0900

    20230412_reviewByFunatsu
    閉じカッコの位置はとても大切です。長いコードで字下げがズレていると、メソッドの範囲が分かりにくくなり作業効率が落ちてしまいます。

--- src/chapter16/task16_2/Hero.java
+++ src/chapter16/task16_2/Hero.java
@@ -4,10 +4,10 @@ public class Hero {
 	private String name;
 	public Hero(String name) { 
 		this.name = name;
-		}
+		}    // Funatsu: 細かいですが閉じカッコの字下げがズレています
 	
 	public String getName() {
 		return this.name;
-		}
+		}    // Funatsu: 細かいですが閉じカッコの字下げがズレています
 
 }
