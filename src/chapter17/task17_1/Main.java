8a89b324e0a42eda4e407457be7b074676b49a2a
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-04-13 10:33:39 +0900

    20230412_reviewByFunatsu

--- src/chapter17/task17_1/Main.java
+++ src/chapter17/task17_1/Main.java
@@ -5,13 +5,13 @@ package chapter17.task17_1;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			String s = null;
+			String s = null;    // Funatsu: ←字下げがズレています
 		    System.out.println(s.length());
 		    int i = Integer.parseInt("三");
 		    
 		} catch (NullPointerException e) {
 			System.out.println("NullPointerException例外をcatchしました。");
-			System.out.println("--ステックトレース（ここから）--");
+			System.out.println("--ステックトレース（ここから）--");    // Funatsu: 細かいですが、「スタックトレース」が正しいです
 			e.printStackTrace();
 			System.out.println("--スタックトレース（ここまで）--");
 		} catch(NumberFormatException n) {
