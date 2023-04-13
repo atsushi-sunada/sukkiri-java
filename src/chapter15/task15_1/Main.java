ce162f87f900ac0d5430c22d37affbc2e77b8061
funatti055 <52117097+funatti055@users.noreply.github.com>
2023-04-13 10:43:44 +0900

    20230412_reviewByFunatsu
    演算子の前後や配列のデータ型の右側に空白を入れるよう、自分が書くコードに統一性を持たせましょう。
    プロジェクト内で書き方にバラツキが生じると、アプリケーション全体でコードが読みにくくなる可能性があります。
    （「細かい話ばかり・・」と思われるかもしれませんが、現場に出てしばらくすると必ず分かります）

--- src/chapter15/task15_1/Main.java
+++ src/chapter15/task15_1/Main.java
@@ -4,13 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<100; i++) {
+		for(int i=0; i<100; i++) {    // Funatsu: =と<の両脇は半角空白を入れましょう
 			sb.append(i+1).append(",");
 			
 		}
 		
 		String s = sb.toString();
-		String[]a = s.split(",");
+		String[]a = s.split(",");    // Funatsu: String[]と変数の間には半角空白を入れましょう。データ型と変数の境目として空白がないと、コードがわかりにくくなる恐れがあります。
 	}
 
 }
